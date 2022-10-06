2a3ec207fafaf6da6a0c8993b0c5849d019a0822
isidora <isidora.poblete.sepulveda@alumnos.sip.cl>
2022-10-06 08:50:53 -0300

    onclickup

---
+++ onclick/jsonclick.js
@@ -0,0 +1,9 @@
+function LogOut(element) {
+    element.innerText = "Logout";
+}
+function EliminarBTN(element) {
+    element.remove();
+}
+function aNinjaLeGusta() {
+    alert("Ninja was liked!");
+}
